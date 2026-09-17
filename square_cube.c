005d644b9312c5e08d7a935fa402ffe0b751ff73
tahahaider721412-ops <tahahaider721412@gmail.com>
2026-09-17 22:46:10 +0500

    Create square_cube.c

---
+++ square_cube.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+int number, square, cube;
+printf("enter an integer: ");
+scanf("%d", &number);
+square = number*number
+cube =number * number*number;
+printf("square = %d\n", square);
+printf("cube = %d", cube);
+ return 0;
+}
